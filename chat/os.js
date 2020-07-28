29c37c3cdecfb3d5d8f1c06024a1ed07c3a0aa9d
foxscallion11 <foxscallion@gmail.com>
2020-07-28 18:12:40 +0800

    Site updated: 2020-07-28 18:12:40

--- chat/os.js
+++ chat/os.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>/**
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>/**
  * 浏览器解析，浏览器、Node.js皆可
  * https://github.com/mumuy/browser
  */
